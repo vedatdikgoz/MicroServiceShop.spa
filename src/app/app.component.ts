0929c6413d684cd1172bdf7686b55126afd7355e
vedat dikgöz <vedatdikgoz@hotmail.com>
2024-08-04 23:18:36 +0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CatalogComponent } from "./features/catalog/catalog.component";
+import { CarouselComponent } from "./carousel/carousel.component";
+import { CategoryComponent } from "./category/category.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CatalogComponent, CarouselComponent, CategoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

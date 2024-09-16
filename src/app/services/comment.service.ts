2a2dc81da170251ffd3b54c9c605403087abdb4c
vedat dikgöz <vedatdikgoz@hotmail.com>
2024-09-17 02:19:58 +0300

    add comment count signalr

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,15 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { UserComment } from '../models/comment/userComment';
 import { environment } from '../environments/environment';
+import * as signalR from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
+  public connection: signalR.HubConnection;
+  // Gelen değeri saklayacak BehaviorSubject
+  private commentCounterSubject = new BehaviorSubject<number>(0);
+  // Bileşenlerde abone olunacak Observable
+  public commentCounter$ = this.commentCounterSubject.asObservable();
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+    // Initialize the SignalR connection
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl("http://localhost:5018/commenthub")
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    this.connection.on("ReceiveCommentCount", (message: number) => {
+      // Değeri BehaviorSubject üzerinden yayınla
+      this.commentCounterSubject.next(message);
+      console.log("Gelen Mesaj :", message);
+    });
+
+
+  }
+
+ // Start connection
+public async start() {
+  try {
+    await this.connection.start();
+    console.log("Hub a bağlandı");
+
+    // Now that the connection is established, invoke the method
+    this.connection.invoke("SendCommentCount")
+      .catch(err => console.error("Error invoking ReceiveMessage:", err));
+  } catch (error) {
+    console.log("Error while starting connection:", error);
+  }
+}
+
+
 
   private baseUrl = `${environment.gatewayBaseUri}/${environment.commentPath}`;
 
